7531522eaea472129a0b0b96b7e2a552740aeac8
piotr-kalanski <piotr.kalanski@gmail.com>
2018-08-13 10:40:22 +0200

    - small improvements in template for tests

--- exercises-java/src/test/java/aaa_template/SolutionTests.java
+++ exercises-java/src/test/java/aaa_template/SolutionTests.java
@@ -10,6 +10,26 @@ public class SolutionTests {
     public void test1() {
         // TODO - change calls:
         runTest("cdcd", 5);
+        /*
+
+        runTest(
+                5,
+                92,
+                23,
+                new int[][] {
+                        new int[] {2, 1},
+                        new int[] {5, 3},
+                        new int[] {5, 1},
+                        new int[] {3, 4},
+                        new int[] {3, 1},
+                        new int[] {5, 4},
+                        new int[] {4, 1},
+                        new int[] {5, 2}
+                },
+                184
+        );
+
+         */
     }
 
     @Test
@@ -26,9 +46,13 @@ public class SolutionTests {
 
     // TODO - change input expected output type:
     private void runTest(String input, int expectedOutput) {
+    //private void runTest(String input, String expectedOutput) {
         System.out.println("\n\nINPUT:");
         // TODO - change println input - if two
         System.out.println(input);
+        //System.out.println(input2);
+        //System.out.println(input3);
+        //System.out.println(Arrays.toString(input));
 
         // TODO - change method:
         int output = Solution.exercise(input);
